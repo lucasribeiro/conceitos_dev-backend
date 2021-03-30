a9fc5591d69370cb6c6d0c48c27012de38b9bec9
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2021-03-29 22:16:34 -0300

    Tipos de Parametros

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+
+/**
+* Tipos de parâmetros
+
+- Query Params: Filtros e paginação
+- Route Params: Identificar recursos (Atualizar ou Deletar)
+- Request Body: Conteúdo na hora de criaar ou editar um recurso
+*/
+
 app.get('/projects', (request, response) => {
+    const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -10,6 +26,10 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+    const {title, owner} = request.body;
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -18,6 +38,9 @@ app.post('/projects', (request, response) => {
 } );
 
 app.put('/projects/:id', (request, response) => {
+    const {id } =request.params;
+    console.log(id);
+
     return response.json([
         'Projeto 4',
         'Projeto 2',
